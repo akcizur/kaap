c739751a07d84b2d351ef85ab352b83ae50b2b95
vics <ruzickajakub95@gmail.com>
2026-09-24 07:51:48 +0200

    Load Google Sans font weights

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/google-sans/400.css"
+import "@fontsource/google-sans/500.css"
+import "@fontsource/google-sans/600.css"
+import "@fontsource/google-sans/700.css"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
