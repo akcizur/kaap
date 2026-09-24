0617bcf3dbbfa19a8f842671bd3c7cd16436a111
vics <ruzickajakub95@gmail.com>
2026-09-24 08:41:18 +0200

    Load Patrick Hand regular font

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import "@fontsource/google-sans/400.css"
 import "@fontsource/google-sans/500.css"
 import "@fontsource/google-sans/600.css"
 import "@fontsource/google-sans/700.css"
-import "@fontsource/sriracha/400.css"
+import "@fontsource/patrick-hand/400.css"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
