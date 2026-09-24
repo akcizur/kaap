911d3982ad4e8e1a81587b2d6a52d851408b2568
vics <ruzickajakub95@gmail.com>
2026-09-24 08:06:40 +0200

    Load Caveat Brush locally

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "@fontsource/google-sans/400.css"
 import "@fontsource/google-sans/500.css"
 import "@fontsource/google-sans/600.css"
 import "@fontsource/google-sans/700.css"
+import "@fontsource/caveat-brush/400.css"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
