43818d5d406cb6de1f4426ad150742df2763d2bb
vics <ruzickajakub95@gmail.com>
2026-09-24 18:23:55 +0200

    Replace secondary font with Fragment Mono

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import "@fontsource/google-sans/400.css"
 import "@fontsource/google-sans/500.css"
 import "@fontsource/google-sans/600.css"
 import "@fontsource/google-sans/700.css"
-import "@fontsource/museomoderno/400.css"
+import "@fontsource/fragment-mono/400.css"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
